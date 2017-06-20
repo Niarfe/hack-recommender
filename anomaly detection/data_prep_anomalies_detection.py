eddf26b9b73bb43f8947b1bb92988f61050cc4b7
GraceZhouHG <grace.zhou@hgdata.com>
2017-06-20 10:37:40 -0700

    add more formulas

--- anomaly detection/data_prep_anomalies_detection.py
+++ anomaly detection/data_prep_anomalies_detection.py
@@ -57,7 +57,6 @@ def get_outliers(db):
     for product_id in product_ids:
         try:
             FN = '/Users/gracezhou/PycharmProjects/data_ops/detect_anomalies/data/{}.csv'.format(product_id)
-            print 'found ', product_id
             dataframe = pd.read_table(FN, sep=',')
             Q3, Q1 = np.percentile(dataframe[' number of hits'], [75, 25])
             iqr = Q3 - Q1
@@ -69,9 +68,9 @@ def get_outliers(db):
             # print dates
             for i in range(len(num_prod_hits)):
                 print num_prod_hits[i] > upper_limit or num_prod_hits[i] < lower_limit
-                print num_prod_hits[i]
-                print upper_limit
-                print lower_limit
+                # print num_prod_hits[i]
+                # print upper_limit
+                # print lower_limit
                 if (num_prod_hits[i] > upper_limit) or (num_prod_hits[i] < lower_limit):
                     print product_ids[product_id], dates[i], num_prod_hits[i]
                     of.write('{}\t{}\t{}\n'.format(product_ids[product_id], dates[i], num_prod_hits[i]))
@@ -79,8 +78,83 @@ def get_outliers(db):
             print err
 
 
+def get_outliers_with_z_score(db):
+    product_ids = get_product_ids(db)
+    OF = 'output/detected_outlier_z_score.txt'
+    of = open(OF, mode='a+')
+    of.write('product_id\tdate\tnum_prod_hits\n')
+    for product_id in product_ids:
+        try:
+            FN = '/Users/gracezhou/PycharmProjects/data_ops/detect_anomalies/data/{}.csv'.format(product_id)
+            dataframe = pd.read_table(FN, sep=',')
+            threshold = 3
+            mean_y = np.mean(dataframe[' number of hits'])
+            stdev_y = np.std(dataframe[' number of hits'])
+            num_prod_hits = list(dataframe[' number of hits'])
+            dates = list(dataframe['date'])
+            # print num_prod_hits
+            # print dates
+            for i in range(len(num_prod_hits)):
+                print np.abs((num_prod_hits[i] - mean_y)/stdev_y) > threshold
+                if np.abs((num_prod_hits[i] - mean_y)/stdev_y) > threshold:
+                    # print product_ids[product_id], dates[i], num_prod_hits[i]
+                    of.write('{}\t{}\t{}\n'.format(product_ids[product_id], dates[i], num_prod_hits[i]))
+        except IOError as err:
+            print err
+
+
+def get_outliers_with_modified_z_score(db):
+    product_ids = get_product_ids(db)
+    OF = 'output/detected_outlier_modified_z_score.txt'
+    of = open(OF, mode='a+')
+    of.write('product_id\tdate\tnum_prod_hits\n')
+    for product_id in product_ids:
+        try:
+            FN = '/Users/gracezhou/PycharmProjects/data_ops/detect_anomalies/data/{}.csv'.format(product_id)
+            dataframe = pd.read_table(FN, sep=',')
+            threshold = 3.5
+            median_y = np.median(dataframe[' number of hits'])
+            num_prod_hits = list(dataframe[' number of hits'])
+            dates = list(dataframe['date'])
+            median_abs_dev_y = np.median([np.abs(y - median_y) for y in num_prod_hits])
+            # print num_prod_hits
+            # print dates
+            for i in range(len(num_prod_hits)):
+                print np.abs(0.6745 * (num_prod_hits[i] - median_y) / median_abs_dev_y) > threshold
+                if np.abs(0.6745 * (num_prod_hits[i] - median_y) / median_abs_dev_y) > threshold:
+                    # print product_ids[product_id], dates[i], num_prod_hits[i]
+                    of.write('{}\t{}\t{}\n'.format(product_ids[product_id], dates[i], num_prod_hits[i]))
+        except IOError as err:
+            print err
+
+
+def get_outliers_with_e_score(db):
+    product_ids = get_product_ids(db)
+    OF = 'output/detected_outlier_modified_z_score.txt'
+    of = open(OF, mode='a+')
+    of.write('product_id\tdate\tnum_prod_hits\n')
+    for product_id in product_ids:
+        try:
+            FN = '/Users/gracezhou/PycharmProjects/data_ops/detect_anomalies/data/{}.csv'.format(product_id)
+            dataframe = pd.read_table(FN, sep=',')
+            Q3, Q1 = np.percentile(dataframe[' number of hits'], [75, 25])
+            iqr = Q3 - Q1
+            median_y = np.median(dataframe[' number of hits'])
+            num_prod_hits = list(dataframe[' number of hits'])
+            dates = list(dataframe['date'])
+            threshold = 1.75
+            # print num_prod_hits
+            # print dates
+            for i in range(len(num_prod_hits)):
+                print np.abs((num_prod_hits[i] - median_y) / iqr) > threshold
+                if np.abs((num_prod_hits[i] - median_y) / iqr) > threshold:
+                    # print product_ids[product_id], dates[i], num_prod_hits[i]
+                    of.write('{}\t{}\t{}\n'.format(product_ids[product_id], dates[i], num_prod_hits[i]))
+        except IOError as err:
+            print err
+
 if __name__ == "__main__":
     db = mysql.connector.connect(db='matcher', host='PC4', user='grace', passwd='2DM1YG6SrQUW4yg6')
     # write_prod_hits_to_file(db)
-    get_outliers(db)
+    get_outliers_with_modified_z_score(db)
 
